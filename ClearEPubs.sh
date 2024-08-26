730d8aa66c53b07217bfbfce22ce40f89a72271f
DynamoRocket <brandonmcaulkins@mailfence.com>
2024-08-26 10:11:51 -0400

    Commit

--- ClearEPubs.sh
+++ ClearEPubs.sh
@@ -3,9 +3,9 @@
 #A script I made to clear certain added pages to certain ePubs I own
 #You will need to have zip installed for this
 #Just drop in the folder with the ePubs. It will search recursively as well.
-#hmm
 
 #This puts the results of the find command into an array
+#This command is linux black magic. How it works, I dunno, but it does.
 mapfile -t FileList < <(find "$PWD" -type f -name "*.epub")
 
 for File in "${FileList[@]}"
